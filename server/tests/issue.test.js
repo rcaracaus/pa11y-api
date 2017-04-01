e0f41c9e0bc2a7e436616ef0be454409719a24ec
Robert Caracaus <rcaracaus@gmail.com>
2017-04-01 12:57:24 -0600

    Added tests for should respect code and reportId parameters and should respect limit and skip parameters

--- server/tests/issue.test.js
+++ server/tests/issue.test.js
@@ -6,9 +6,42 @@ import app from '../../index';
 
 chai.config.includeStack = true;
 
-/**
- * root level hooks
- */
+
+let issuesData = [
+  {
+    code: 'imageMissingAlt',
+    context: '<img src="/asdf.jgp" />',
+    message: 'Different code and reportId',
+    selector: 'html > head > img',
+    type: 'warning',
+    typeCode: '3',
+    reportId: 'c5fe44825b658dd2c6038880',
+    url: 'url'
+  },
+  {
+    code: 'inaccessibleTitle',
+    context: '<title>The worst accessibility problem ever...</title>',
+    message: 'Check that the title element describes the document.',
+    selector: 'html > head > title',
+    type: 'notice',
+    typeCode: '3',
+    reportId: '58dd2c6038880c5fe44825b6',
+    url: 'url'
+  },
+  {
+    code: 'imageMissingAlt',
+    context: '<img src="/asdf.jgp" />',
+    message: 'Check that the image has alt text.',
+    selector: 'html > head > img',
+    type: 'warning',
+    typeCode: '3',
+    reportId: '58dd2c6038880c5fe44825b6',
+    url: 'url'
+  }
+];
+
+let lastIssue = issuesData.slice(-1)[0];
+
 after((done) => {
   // required because https://github.com/Automattic/mongoose/issues/1251#issuecomment-65793092
   mongoose.models = {};
@@ -18,34 +51,35 @@ after((done) => {
 });
 
 describe('## Issue APIs', () => {
-  let issue = {
-    code: 'test',
-    context: '<title>WordPress.com: Create a free we...</title>',
-    message: 'Check that the title element describes the document.',
-    selector: 'html > head > title',
-    type: 'notice',
-    typeCode: '3',
-    reportId: '58dd2c6038880c5fe44825b6',
-    url: 'url'
-  };
-  let issue2;
 
   describe('# POST /api/issues', () => {
+
+    it('should have test data', (done) => {
+      for (let i = 0; i < issuesData.length - 1; i++) {
+        request(app)
+          .post('/api/issues')
+          .send(issuesData[i])
+          .then((res) => {
+            issuesData[i] = res.body;
+          });
+      }
+      done();
+    });
     it('should create a new issue', (done) => {
       request(app)
         .post('/api/issues')
-        .send(issue)
+        .send(lastIssue)
         .expect(httpStatus.OK)
         .then((res) => {
-          expect(res.body.code).to.equal(issue.code);
-          expect(res.body.context).to.equal(issue.context);
-          expect(res.body.message).to.equal(issue.message);
-          expect(res.body.selector).to.equal(issue.selector);
-          expect(res.body.context).to.equal(issue.context);
-          expect(res.body.type).to.equal(issue.type);
-          expect(res.body.reportId).to.equal(issue.reportId);
-          expect(res.body.url).to.equal(issue.url);
-          issue = res.body;
+          expect(res.body.code).to.equal(lastIssue.code);
+          expect(res.body.context).to.equal(lastIssue.context);
+          expect(res.body.message).to.equal(lastIssue.message);
+          expect(res.body.selector).to.equal(lastIssue.selector);
+          expect(res.body.context).to.equal(lastIssue.context);
+          expect(res.body.type).to.equal(lastIssue.type);
+          expect(res.body.reportId).to.equal(lastIssue.reportId);
+          expect(res.body.url).to.equal(lastIssue.url);
+          lastIssue = res.body;
           done();
         })
         .catch(done);
@@ -53,31 +87,41 @@ describe('## Issue APIs', () => {
   });
 
   describe('# GET /api/issues', () => {
+
     it('should get issue listing', (done) => {
-      // newly installed versions may have a new DB
-      // we need to post an issue first to see a list (more than one)
-      issue2 = issue;
       request(app)
-        .post('/api/issues')
-        .send(issue2)
-        .then((res) => {
-          issue2 = res.body;
-          request(app)
-            .get('/api/issues')
-            .expect(httpStatus.OK);
-        })
+        .get('/api/issues')
+        .expect(httpStatus.OK)
         .catch(done);
       done();
     });
 
-    it('should respect limit and query parameter', (done) => {
+    it('should respect limit and skip parameters', (done) => {
+      let secondIssueId;
       request(app)
         .get('/api/issues')
-        .query({ limit: 1, skip: 1 })
-        .expect(httpStatus.OK)
+        .then((res) => {
+          secondIssueId = res.body[1]._id;
+          return request(app)
+            .get('/api/issues')
+            .query({ limit: 1, skip: 1 })
+            .expect(httpStatus.OK);
+        })
         .then((res) => {
           expect(res.body.length).to.equal(1);
-          expect(res.body[0]._id).to.equal(issue._id);
+          expect(res.body[0]._id).to.equal(secondIssueId);
+          done();
+        })
+        .catch(done);
+    });
+
+    it('should respect code and reportId parameters', (done) => {
+      request(app)
+        .get('/api/issues')
+        .query({ code: 'imageMissingAlt', reportId: 'c5fe44825b658dd2c6038880' })
+        .expect(httpStatus.OK)
+        .then((res) => {
+          expect(res.body[0].message).to.equal('Different code and reportId');
           done();
         })
         .catch(done);
@@ -98,17 +142,17 @@ describe('## Issue APIs', () => {
   describe('# GET /api/issues/:issueId', () => {
     it('should get issue details', (done) => {
       request(app)
-        .get(`/api/issues/${issue._id}`)
+        .get(`/api/issues/${lastIssue._id}`)
         .expect(httpStatus.OK)
         .then((res) => {
-          expect(res.body.code).to.equal(issue.code);
-          expect(res.body.context).to.equal(issue.context);
-          expect(res.body.message).to.equal(issue.message);
-          expect(res.body.selector).to.equal(issue.selector);
-          expect(res.body.context).to.equal(issue.context);
-          expect(res.body.type).to.equal(issue.type);
-          expect(res.body.reportId).to.equal(issue.reportId);
-          expect(res.body.url).to.equal(issue.url);
+          expect(res.body.code).to.equal(lastIssue.code);
+          expect(res.body.context).to.equal(lastIssue.context);
+          expect(res.body.message).to.equal(lastIssue.message);
+          expect(res.body.selector).to.equal(lastIssue.selector);
+          expect(res.body.context).to.equal(lastIssue.context);
+          expect(res.body.type).to.equal(lastIssue.type);
+          expect(res.body.reportId).to.equal(lastIssue.reportId);
+          expect(res.body.url).to.equal(lastIssue.url);
           done();
         })
         .catch(done);
@@ -128,14 +172,13 @@ describe('## Issue APIs', () => {
 
   describe('# PUT /api/issues/:issueId', () => {
     it('should update issue details', (done) => {
-      issue.code = 'KK';
+      lastIssue.code = 'KK';
       request(app)
-        .put(`/api/issues/${issue._id}`)
-        .send(issue)
+        .put(`/api/issues/${lastIssue._id}`)
+        .send(lastIssue)
         .expect(httpStatus.OK)
         .then((res) => {
           expect(res.body.code).to.equal('KK');
-          expect(res.body.context).to.equal(issue.context);
           done();
         })
         .catch(done);
@@ -157,20 +200,23 @@ describe('## Issue APIs', () => {
 
   describe('# DELETE /api/issues/', () => {
     it('should delete issue', (done) => {
+      // Delete all test data.
+      for (let i = 0; i < issuesData.length - 1; i++) {
+        let issue = issuesData[i];
+        request(app)
+          .delete(`/api/issues/${lastIssue._id}`);
+      }
+      // Test final deletion.
+      let issue = issuesData[issuesData.length];
       request(app)
-        .delete(`/api/issues/${issue._id}`)
+        .delete(`/api/issues/${lastIssue._id}`)
         .expect(httpStatus.OK)
         .then((res) => {
           expect(res.body.code).to.equal('KK');
-          expect(res.body.context).to.equal(issue.context);
+          expect(res.body.context).to.equal(lastIssue.context);
           done();
         })
-        // delete issue2
-        .then(() => request(app)
-          .delete(`/api/issues/${issue2._id}`)
-          .expect(httpStatus.OK)
-          .catch(done)
-        );
+        .catch(done);
     });
   });
 });
